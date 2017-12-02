b9a875c0b83f5311089fea354eaf23ea6e98a8be
vnyk <uservks@gmail.com>
2017-12-02 17:44:48 +0530

    initialdds

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os, sys
 from flask import Flask, request
-from utils import wit_response, get_news_elements
 from pymessenger import Bot
 
 app = Flask(__name__)
@@ -20,6 +19,31 @@ def verify():
     return "Hello world", 200
 
 
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					# Echo
+					response = messaging_text
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
 
 
 def log(message):
